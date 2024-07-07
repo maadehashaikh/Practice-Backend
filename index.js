7ccaa9b01a0d5ec5fa8ed3b1cd3ee1784a74ad77
Maadeha shaikh <maadeham@gmail.com>
2024-07-07 18:55:40 +0500

    Updated !!

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-
+const path = require('path');
 app.use(express.json()); 
 app.use(express.urlencoded({extended:true}));   // in 2 lines ko we say parser 
-
+app.use(express.static(path.join(__dirname , 'public'))); //basically we're trying to say kay har request kay liyay static files yahn dhoondna 
+app.set('view engine','ejs');
 app.get("/",function(req,res){
-   res.send("Chal rha h !!");
+   res.render("index");
 })
-
 app.listen(3000);
+
+// console.log(__dirname);
